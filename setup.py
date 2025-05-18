1aa438c1264b9da18ab734ce1ea2107a30057563
= <aaron.oster28@gmail.com>
2025-05-17 20:55:17 -0400

    added basic flask app functions

---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py (in your root folder)
+from setuptools import setup, find_packages
+
+setup(
+    name="voice_chess",
+    version="0.1",
+    packages=find_packages(),
+)
---
+++ web/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from game.lichess_game import LichessVoiceGame
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+game_instance = None
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('start_game')
+def handle_start_game():
+    global game_instance
+    game_instance = LichessVoiceGame()
+    game_instance.main(debug=False)
+    return {'status': 'Game started'}
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
